776638ed9e5b15fc7ef20f0f44cfe7b0527af6ac
Homer Rich <hrich@admin1.pkilab.disa.mil>
2019-11-26 16:40:08 +0000

    Fixing up some syntax and including an empty argument as a help option

--- bundleToNSS.sh
+++ bundleToNSS.sh
@@ -2,10 +2,11 @@
 # Author: Homer Rich
 # Use: Convert P7 bundle from cyber.mil and load it into an NSS database.
 
-if [ $1 == "help" ] || [ $1 == "-h" ] || [ $1 == "--help" ]; then
+if [[ $1 = "help" ]] || [[ $1 = "-h" ]] || [[ $1 = "--help" ]] || [[ -z $1 ]]; then
   printf "###############################################################################\n"
   printf "Run this command to add all certs in a PKCS7(P7) bundle to your NSS store\n"
   printf "Dependencies: nss openssl awk sed grep\n"
+  printf "Arguments: 1:Pem that you would like loaded.  2:NSS location to load in to.\n"
   printf "Example usage:\n"
   printf "\t$ bash bundleToNSS.sh Certificates_PKCS7_v5.5_DOD.pem.p7b ~/alias/nssdb\n" 
   printf "###############################################################################\n"
@@ -14,7 +15,7 @@ fi
 inputCertChain=$(openssl pkcs7 -in $1 -print_certs)
 subjectCount=$(echo "$inputCertChain" | grep "subject" | wc -l)
 touch tempCer
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   for (( i=1 ; i < "$subjectCount" ; i++ )); do
     curPem=$(echo "$inputCertChain" | awk  "/subject=/{n++};n==$i;n==$i+1{exit}")
     curSubject=$(echo "$curPem" | head -1 | sed "s/.*=//g")
